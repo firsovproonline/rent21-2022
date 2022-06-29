d4b8936252cf48e1116c08778421513e615fca4a
Вячеслав Фирсов <vfirsov@stat4market.com>
2022-06-29 07:42:49 +0300

    components realestate

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="main-block">
-
+    <div class="row">
+      <section id="home" class="section-py-space" style="margin-top: 20px;display: block;">
+        <div class="row">
+          <div class="col-12 custom-container">
+            <ListAddress />
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import ListAddress from "~/components/Realestate/Address/listAddress";
+
 export default {
   name: 'IndexPage',
   layout: 'default',
+  components: {ListAddress},
   async asyncData ({ app, route, params, error, store }) {
     try {
       await store.dispatch('realestate/load')
